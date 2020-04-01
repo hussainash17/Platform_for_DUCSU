77fb849a608ce407d29d31749d90ca0c3b4e1d02
ashrafhussain17 <ashrafhussain.2317.csedu@gmail.com>
2020-04-01 23:05:54 +0600

    Profile router done

--- models/User.js
+++ models/User.js
@@ -14,6 +14,13 @@ const UserSchema = new Schema({
   password: {
     type: String,
     required: true
+  },
+  avatar: {
+    type: String
+  },
+  date: {
+    type: Date,
+    default: Date.now
   }
 });
 
