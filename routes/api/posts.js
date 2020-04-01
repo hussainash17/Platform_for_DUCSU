78263750259b9c596f8a280ab3eb1fa602fead02
ashrafhussain17 <ashrafhussain.2317.csedu@gmail.com>
2020-04-01 20:22:07 +0600

    Post route completed and validation added

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -182,6 +182,82 @@ router.post(
 )
 
 
+// @route        POST api/posts/comment/:id
+// @description  Add comment to post
+// @access       Private
+
+router.post(
+    '/comment/:id', passport.authenticate('jwt', {
+        session: false
+    }),
+    (req, res) => {
+        const {errors, isValid} = validatePostInput(req.body)
+
+        if (!isValid){
+            return res.status(400).json(errors)
+        }
+
+        Post.findById(req.params.id)
+        .then(post => {
+            const newComment = {
+                text: req.body.text,
+                name: req.body.name,
+                avatar: req.body.avatar,
+                user: req.body.user
+            }
+
+            // Add new comments to comments array
+            post.comments.unshift(newComment)
+            post.save().then(post => {
+                res.json(post)
+            })
+        })
+        .catch(err => {
+            res.status(404).json({
+                postnotfound: 'No post found'
+            })
+        })
+    }
+)
+
+// @route        DELETE api/posts/comment/:id/:comment_id
+// @description  Remove comment from post
+// @access       Private
+
+router.delete(
+    '/comment/:id/:comment_id',
+    passport.authenticate('jwt', {
+        session: false
+    }),
+    (req, res) => {
+        Post.findById(req.params.id)
+            .then(post => {
+                if(post.comments.filter(
+                    comment => comment._id.toString() === req.params.comment_id
+                    ).length === 0 
+                    )
+                    {
+                        return res.status(404)
+                            .json({
+                                commentnotexists: 'Comment does not exist'
+                            })
+                    }
+
+                    const removeIndex = post.comments
+                        .map(item => item._id.toString())
+                        .indexOf(req.params.comment_id)
+                    
+                    post.comments.splice(removeIndex, 1)
+                    post.save().than(post => res.json(post))
+            })
+            .catch(err => {
+                res.status(404).json({
+                    postnotfound: 'No post found'
+                })
+            })
+    }
+)
+
 
 
 
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -3,6 +3,11 @@ const router = express.Router()
 const mongoose = require('mongoose')
 const passport = require('passport')
 
+const validateProfileInput = require('../../validation/profile')
+const validateExperienceInput = require('../../validation/experience')
+const validateEducationInput = require('../../validation/education')
+
+
 const Profile = require('../../models/Profile')
 
 router.get('/test', (req, res) => res.json({ msg: 'Profile Works' }))
